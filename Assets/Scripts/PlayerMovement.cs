6cbaaecc3947167095efd8214b75763c3540772e
Cristian067 <cristian.n.c005@gmail.com>
2024-09-24 13:47:30 +0200

    hookpain

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
 using UnityEngine;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
@@ -9,19 +11,27 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
 
+
+    [SerializeField] private float hookDistance;
+
+    
+
     private float horizontal;
 
 
     private Rigidbody2D rb;
-    
-    
-    
+
+    [SerializeField]private Camera cam;
+
+
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
 
@@ -31,8 +41,15 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
 
+        Vector3 mousePos = Input.mousePosition;  // Set(transform.position.x,transform.position.y,transform.position.z);
 
+        Debug.Log(transform.localPosition);
+        
+            
         horizontal = Input.GetAxis("Horizontal");
+
+
+        
         /*
         
         
@@ -44,6 +61,14 @@ public class PlayerMovement : MonoBehaviour
         }
 
         */
+        Vector3 relativePosition;
+
+        relativePosition = cam.transform.TransformDirection(transform.position - cam.transform.position);
+
+        Vector3 originPos = cam.ScreenPointToRay(relativePosition).origin;
+        Vector3 destinyPos = cam.ScreenPointToRay(mousePos).origin;
+
+        Debug.DrawLine(originPos, destinyPos);
 
 
         if (Input.GetKeyDown("w"))
@@ -54,14 +79,35 @@ public class PlayerMovement : MonoBehaviour
 
         }
 
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            
+            RaycastHit2D hit = Physics2D.Raycast(destinyPos, originPos, hookDistance);
+            Debug.DrawLine(originPos, hit.point);
+            if (hit.collider != null)
+            {
+                //Debug.Log("sha");
+                //Hook(hit);
+            }
+
+        }
+
         
         
     }
     private void FixedUpdate()
-
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
+    }
+
+
+    private void Hook(RaycastHit2D hit)
+    {
+        transform.Translate(hit.transform.position);
 
     }
+
+
 }
