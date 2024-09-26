c149abaeb2b24452c2fe4123a17107be23652bcf
Cristian067 <cristian.n.c005@gmail.com>
2024-09-26 10:20:48 +0200

    26/09

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,40 +10,26 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    [SerializeField]private Camera cam;
 
     [SerializeField] private float speed;
     [SerializeField] private float jumpForce;
 
     [SerializeField] private bool inHook;
+    [SerializeField] private bool isOnGround;
 
 
     [SerializeField] private float hookDistance;
-
     [SerializeField] private float hookForce;
 
+    //[SerializeField] private float currentSpeed;
 
     public LayerMask layerToHit;
 
     private float horizontal;
-    private int horizontalInt;
-
-
-    private Rigidbody2D rb;
-
-    [SerializeField]private Camera cam;
-
 
     private Vector3 hookedPos;
-
-    public float minimum = -1.0F;
-    public float maximum = 1.0F;
-
-    // starting value for the Lerp
-    static float t = 0.0f;
-
-
-
-
 
     // Start is called before the first frame update
     void Start()
@@ -53,30 +39,33 @@ public class PlayerMovement : MonoBehaviour
         
     }
 
-
-
-
     // Update is called once per frame
     void Update()
     {
-
+        Flip();
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             
         horizontal = Input.GetAxis("Horizontal");
 
-        horizontalInt = (int)horizontal;
+        //horizontalInt = (int)horizontal;
 
+        RaycastHit2D hitGround = Physics2D.Raycast(transform.position + new Vector3(0, -0.51f, 0), Vector3.down,0.1f);
+        //Debug.DrawLine(transform.position + new Vector3(0,-0.51f,0), transform.position + new Vector3(0,-0.61f,0));
 
-        
+        if (hitGround)
+        {
+            isOnGround = true;
+        }
+        else if(!hitGround)
+        {
+            isOnGround= false;
+        }
 
         /*
         if (horizontal != 0)
         {
             transform.Translate(new Vector3(1,0,0) * horizontal * speed * Time.deltaTime);
-
-            
         }
-
         */
 
         Vector3 direction = (mousePos - transform.position).normalized;
@@ -84,16 +73,15 @@ public class PlayerMovement : MonoBehaviour
         //DrawLine(transform.position, mousePos, Color.blue);
 
 
-        if (Input.GetKeyDown("w"))
+        if (Input.GetKeyDown("w") && isOnGround)
         {
 
             //transform.Translate(new Vector3(0,1,0) * jumpForce);
+            rb.velocity = new Vector2(rb.velocity.x, 0);
             rb.AddForce(transform.up * jumpForce);
+            isOnGround = false;
 
         }
-
-        
-
 
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
@@ -106,41 +94,47 @@ public class PlayerMovement : MonoBehaviour
                 Debug.Log(hit.collider.name);
                 Hook(hit, direction);
             }
-
         }
-
-
-        t += 0.5f * Time.deltaTime;
-        if (!inHook)
+        /*
+        if(currentSpeed > speed)
+        {
+            currentSpeed= speed;
+        }
+        if (currentSpeed < -speed)
+        {
+            currentSpeed = -speed;
+        }
+        */
+        if (!inHook )
         {
             rb.velocity = new Vector2(Mathf.MoveTowards(rb.velocity.x,0f,15f * Time.deltaTime), rb.velocity.y); 
+            //currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, 15f * Time.deltaTime);
 
         }
            //new Vector2(Mathf.Lerp(minimum, maximum, 0), rb.velocity.y);
-
-        
-
+           /*
+        if (currentSpeed != 0f)
+        {
+            rb.velocity = new Vector2(currentSpeed, rb.velocity.y);
+        }
+           */
 
     }
     private void FixedUpdate()
     {
-        
         if (Input.GetKey(KeyCode.A) && !inHook || Input.GetKey(KeyCode.D) && !inHook)
         {
-
-
             rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
         }
         /*
-        if (rb.velocity.x >= 0)
+        if(Input.GetKey("a") && !inHook)
         {
-            rb.velocity = new Vector2(rb.velocity.x -0.05f, rb.velocity.y);
+            currentSpeed += -0.5f;
         }
-        else if (rb.velocity.x <= 0)
+        if (Input.GetKey("d") && !inHook)
         {
-            rb.velocity = new Vector2(rb.velocity.x + 0.05f, rb.velocity.y);
+            currentSpeed += 0.5f;
         }
-
         */
     }
 
@@ -152,18 +146,34 @@ public class PlayerMovement : MonoBehaviour
         rb.AddForce(direction * hookForce, ForceMode2D.Impulse);
         Debug.Log(hit.collider.gameObject.transform.position);
         hookedPos = hit.collider.transform.position;
+        isOnGround = false;
         StartCoroutine("CooldownHook");
-
         //transform.Translate(hit.transform.position);
-
     }
 
 
     private IEnumerator CooldownHook()
     {
-        yield return new WaitForSeconds(0.2f);
+        yield return new WaitForSeconds(0.3f);
         inHook = false;
     }
 
+    private void Attack()
+    {
+
+    }
+
+    private void Flip()
+    {
+        if (rb.velocity.x < 0f)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x,180,transform.rotation.z);
+        }
+
+        if (rb.velocity.x > 0f)
+        {
+            transform.rotation = Quaternion.Euler(transform.rotation.x, 0, transform.rotation.z);
+        }
+    }
 
 }
