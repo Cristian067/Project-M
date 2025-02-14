f2cbd6d3603c83c046870e1c262b70d2060d5fc3
Cristian067 <cristian.n.c005@gmail.com>
2025-02-14 12:48:56 +0100

    14/02

---
+++ Assets/Scripts/Triggers/TriggerAnimation.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class TriggerAnimation : MonoBehaviour
+{
+
+    [SerializeField] private bool isOnTrigger;
+    [SerializeField] private bool isOnCollision;
+
+    [SerializeField] private Animator animator;
+    [SerializeField] private Animator targetAnimator;
+
+    [SerializeField] private string tagString;
+    [SerializeField] private int layerIdToUse;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Burn()
+    {
+
+        Destroy(gameObject);
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Debug.Log(collision.gameObject.name + "tr");
+        if (collision.gameObject.layer == layerIdToUse)
+        {
+            //Burn();
+            animator.SetTrigger("burn");
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Debug.Log(collision.gameObject.layer + "col  " + layerIdToUse);
+        if (collision.gameObject.layer == layerIdToUse)
+        {
+            //Burn();
+            animator.SetTrigger("burn");
+        }
+
+    }
+
+    private void OnDestroy()
+    {
+        if (isOnTrigger && targetAnimator != null)
+        {
+            targetAnimator.SetTrigger("trigger");
+            
+        }
+        
+        if (isOnCollision && targetAnimator != null)
+        {
+            targetAnimator.SetTrigger("trigger");
+            
+        }
+        
+    }
+
+
+
+
+}
